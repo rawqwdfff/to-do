eb3fc2197fa5cfd72d448801c124c4701a2582e8
rawqwdfff <natha.1@iitj.ac.in>
2023-08-01 23:41:35 +0530

    Update website URL

--- app.js
+++ app.js
@@ -128,6 +128,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.WEBSITE_URL, function() {
   console.log("Server started on port 3000");
 });
