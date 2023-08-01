7fedb9caeb00e9f71b24c26d4e4cba7ad3b434ef
rawqwdfff <natha.1@iitj.ac.in>
2023-08-02 00:52:46 +0530

    Update port

--- app.js
+++ app.js
@@ -128,6 +128,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.WEBSITE_URL, function() {
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
